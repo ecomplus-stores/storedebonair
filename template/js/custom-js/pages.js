17e6a6120bc9cab1ea4f0b588df79c26fd539c1f
eduvlemes <edu.vlemes@gmail.com>
2021-03-01 17:05:29 -0300

    Ajuste mobile

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,11 @@
 // Add your custom JavaScript for storefront pages here.
+
+const isMobile = window.innerWidth < 990 ? true : false;
+if(isMobile){
+    $('#search-bar').appendTo('#search-mobile');
+    $('#top-user').appendTo('#user-mobile')
+}
+if(ecomPassport.getCustomerName() != ""){
+    $('#username_').text(ecomPassport.getCustomerName())
+    $('#functions_').html('Acesse sua <a href="/app/#/account/">conta</a> ou veja seus <a href="href="/app/#/account/orders">pedidos</a>.')
+}
